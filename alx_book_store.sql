21a6494275fd359640c55aad06bb009cb5a871f8
Emmanuel <emmanuelkibe148@gmail.com>
2025-06-30 00:45:49 +0400

    Introduction to database

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -2,7 +2,7 @@ CREATE DATABASE IF NOT EXISTS alx_book_store;
 USE alx_book_store;
 
 
-CREATE TABLE Books(book_id INT PRIMARY KEY, title VARCHAR(130), author_id INT, Price FLOAT, publication_date DATE, FOREIGN KEY (author_id) REFERENCES Authors(author_id));
+CREATE TABLE Books(book_id INT PRIMARY KEY, title VARCHAR(130), author_id INT, price FLOAT, publication_date DATE, FOREIGN KEY (author_id) REFERENCES Authors(author_id));
 CREATE TABLE Authors( author_id INT PRIMARY KEY, author_name VARCHAR(215));
 CREATE TABLE Customers( customer_id INT PRIMARY KEY, customer_name VARCHAR(215), email VARCHAR(215), address TEXT);
 CREATE TABLE Orders(order_id INT PRIMARY KEY, customer_id INT, order_date DATE, FOREIGN KEY (customer_id) REFERENCES Customers(customer_id));
