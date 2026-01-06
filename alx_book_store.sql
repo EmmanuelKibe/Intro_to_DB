af90b174e267ee517be891a9d25148f84f19180c
Emmanuel <emmanuelkibe148@gmail.com>
2026-01-06 22:33:19 +0300

    Refactor table definitions for consistency: standardize casing and correct data types in Authors, Books, Customers, Orders, and Order_Details tables

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -5,40 +5,40 @@ CREATE DATABASE IF NOT EXISTS alx_book_store;
 USE alx_book_store;
 
 CREATE TABLE Authors (
-    Author_ID INT PRIMARY KEY,
-    Author_Name VARCHAR(215)
+    author_id INT PRIMARY KEY,
+    author_name VARCHAR(215)
 );
 
-CREATE TABLE BOOKS (
-    Book_ID INT PRIMARY KEY,
-    Title VARCHAR(130),
-    Author_ID INT,
-    Price FLOAT,
-    Publication_Date DATE,
-    FOREIGN KEY (Author_ID) REFERENCES AUTHORS(Author_ID)
+CREATE TABLE Books (
+    book_id INT PRIMARY KEY,
+    title VARCHAR(130),
+    author_id INT,
+    price FLOAT,
+    publication_date DATE,
+    FOREIGN KEY (author_id) REFERENCES Authors(author_id)
 );
 
-CREATE TABLE CUSTOMER (
-    Customer_ID INT PRIMARY KEY,
-    Customer_NAME VARCHAR(215),
-    Email VARCHAR(215),
-    Address TEXT
+CREATE TABLE Customers (
+    customer_id INT PRIMARY KEY,
+    customer_name VARCHAR(215),
+    email VARCHAR(215),
+    address TEXT
 );
 
-CREATE TABLE ORDERS (
-    Order_ID INT PRIMARY KEY,
-    Customer_ID INT,
-    Order_date DATE,
-    FOREIGN KEY (Customer_ID) REFERENCES CUSTOMER(Customer_ID)
+CREATE TABLE Orders (
+    order_id INT PRIMARY KEY,
+    customer_id INT,
+    order_date DATE,
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
-CREATE TABLE ORDER_DETAILS (
+CREATE TABLE Order_Details (
     OrderdetailID INT PRIMARY KEY,
     Order_ID INT,
     Book_ID INT,
-    Quantity FLOAT,
-    FOREIGN KEY (Order_ID) REFERENCES ORDERS(Order_ID),
-    FOREIGN KEY (Book_ID) REFERENCES BOOKS(Book_ID)
+    Quantity INT NOT NULL,
+    FOREIGN KEY (Order_ID) REFERENCES Orders(Order_ID),
+    FOREIGN KEY (Book_ID) REFERENCES Books(Book_ID)
 );
 
 
